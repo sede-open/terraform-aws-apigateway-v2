51c8c035352820b6ad84c458df9635bb36d3878f
Ferhat Yildiz <100704475+ferhatys@users.noreply.github.com>
2024-03-04 10:32:44 +0100

    map(any) to any

--- variables.tf
+++ variables.tf
@@ -202,7 +202,7 @@ variable "mutual_tls_authentication" {
 # routes and integrations
 variable "integrations" {
   description = "Map of API gateway routes with integrations"
-  type        = map(any)
+  type        = any
   default     = {}
 }
 
